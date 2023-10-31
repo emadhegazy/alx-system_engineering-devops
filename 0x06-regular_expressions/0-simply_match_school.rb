effcc2adc331f0fb6e0fba621cbd60f76c89ba87
emadhegazy <acc.ekh1@gmail.com>
2023-10-31 13:08:14 +0000

    done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #This script accepts one argument and passes it to a regular expression,
 #matching method
-#The regular expression must match School
+#The regular expression must be matched School
 
 puts ARGV[0].scan(/School/).join
